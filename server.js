779a89448ae0c1fbef4eedd59e7b4f5497f64c17
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-05 14:04:15 -0600

    get endpoints for all notes and individual notes

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 'use strict';
 
+const express = require('express');
+
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
+
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = data.find(item => item.id === parseInt(id));
+  res.json(note);
+}); 
+
+
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
 
 console.log('hello world!');
 
