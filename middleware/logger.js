1eb95c39b772785742f1ea6c525c9cae548a0853
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-06 15:25:30 -0600

    formatting date

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,20 @@
 'use strict';
 
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
 const logger = (req, res, next) => {
   const now = new Date();
-  console.log(`${now.toLocaleString()} ${req.method} ${req.url}`);
+  const time = formatAMPM(now);
+  console.log(`${now.getMonth() + 1}-${now.getDate()}-${now.getFullYear()} ${time} ${req.method} ${req.url}`);
   next();
 };
 
