b0c92fb5f99e8553b2c566e52606a9714e90aa54
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-10 12:50:42 -0600

    add first GET route normal case test

--- test/server-test.js
+++ test/server-test.js
@@ -20,27 +20,58 @@ describe('Reality check', function () {
 
 });
 
-describe('Express static', function() {
-  it('GET request "/" should return the index page', function() {
-    return chai.request(app)
-      .get('/')
-      .then(function(res) {
-        expect(res).to.exist;
-        expect(res).to.have.status(200);
-        expect(res).to.be.html;
-      });
+describe('Noteful', function() {
+
+  describe('Express static', function() {
+    it('GET request "/" should return the index page', function() {
+      return chai.request(app)
+        .get('/')
+        .then(function(res) {
+          expect(res).to.exist;
+          expect(res).to.have.status(200);
+          expect(res).to.be.html;
+        });
+    });
   });
+  
+  describe('404 handler', function () {
+  
+    it('should respond with 404 when given a bad path', function () {
+      return chai.request(app)
+        .get('/bad/path')
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(404);
+        });
+    });
+  
+  });
+
+  describe('GET /v1/notes', function() {
+    it('should get list of all notes', function() {
+      return chai.request(app)
+        .get('/v1/notes')
+        .then(function(res) {
+          expect(res).to.exist;
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body.length).to.be.at.least(10);
+
+          const expectedKeys = ['id', 'title', 'content'];
+          res.body.forEach(function(item) {
+            expect(item).to.be.a('object');
+            expect(item).to.include.keys(expectedKeys);
+          });
+
+        });
+    });
+  });
+
+
+
+
+
 });
 
-describe('404 handler', function () {
 
-  it('should respond with 404 when given a bad path', function () {
-    return chai.request(app)
-      .get('/bad/path')
-      .catch(err => err.response)
-      .then(res => {
-        expect(res).to.have.status(404);
-      });
-  });
-
-});
